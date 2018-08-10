cbbc816e42f003dac0a484a3331c761d5d834ac8
Tien Dat <datdta95@gmail.com>
2018-08-10 14:36:22 +0700

    Nhập vào năm cần kiểm tra

---
+++ src/CheckLeapYearDemo.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class CheckLeapYearDemo {
+    public static void main(String[] args) {
+        //Nhập vào năm cần kiểm tra
+        int year;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\t KIỂM TRA NĂM NHUẬN");
+        System.out.println("Nhập vào năm cần kiểm tra:");
+        year = scanner.nextInt();
+    }
+}
