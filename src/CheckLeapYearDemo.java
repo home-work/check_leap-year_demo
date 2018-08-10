48586160cd5d14b85a10504ddb74cb1be9d40ccf
Tien Dat <datdta95@gmail.com>
2018-08-10 14:40:52 +0700

    Kiểm tra năm nhuận

--- src/CheckLeapYearDemo.java
+++ src/CheckLeapYearDemo.java
@@ -8,5 +8,21 @@ public class CheckLeapYearDemo {
         System.out.println("\t KIỂM TRA NĂM NHUẬN");
         System.out.println("Nhập vào năm cần kiểm tra:");
         year = scanner.nextInt();
+
+        //Kiểm tra năm nhuận
+        boolean chiaHetCho4 = year % 4 == 0;
+        boolean chiaHetCho100 = year % 100 == 0;
+        boolean chiaHetCho400 = year % 400 == 0;
+        if (chiaHetCho4) {
+            if (chiaHetCho100) {
+                if (chiaHetCho400) {
+                    System.out.println("Năm nhuận");
+                } else {
+                    System.out.println("Không phải năm nhuận");
+                }
+            } else {
+                System.out.println("Năm nhuận");
+            }
+        } else System.out.println("Không phải năm nhuận");
     }
 }
